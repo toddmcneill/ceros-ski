ea513984de671391c13b37b17b8f741e011dcf64
Todd McNeill <toddmcneill@gmail.com>
2019-09-10 23:48:22 -0600

    Add new lines at the end of each file where they were missing.
    Change everything to 4-space tabs instead of a mix of 2- and 4-space tabs.

--- src/Entities/Rhino.js
+++ src/Entities/Rhino.js
@@ -3,95 +3,95 @@ import { Entity } from "./Entity";
 import { intersectTwoRects, Rect } from "../Core/Utils";
 
 export class Rhino extends Entity {
-  assetName = Constants.RHINO_DEFAULT;
+    assetName = Constants.RHINO_DEFAULT;
 
-  speed = Constants.RHINO_STARTING_SPEED;
+    speed = Constants.RHINO_STARTING_SPEED;
 
-  mode = Constants.RHINO_MODES.CHASE;
-  animationDuration = 0;
+    mode = Constants.RHINO_MODES.CHASE;
+    animationDuration = 0;
 
-  constructor(x, y) {
-    super(x, y);
-  }
+    constructor(x, y) {
+        super(x, y);
+    }
 
-  setMode(mode) {
-    this.mode = mode;
-    this.animationDuration = 0;
-    this.updateAsset();
-  }
+    setMode(mode) {
+        this.mode = mode;
+        this.animationDuration = 0;
+        this.updateAsset();
+    }
 
-  getChaseStep() {
-    return Math.floor(this.animationDuration / Constants.RHINO_CHASE_TIME);
-  }
+    getChaseStep() {
+        return Math.floor(this.animationDuration / Constants.RHINO_CHASE_TIME);
+    }
 
-  getEatStep() {
-    return Math.floor(this.animationDuration / Constants.RHINO_EAT_TIME);
-  }
+    getEatStep() {
+        return Math.floor(this.animationDuration / Constants.RHINO_EAT_TIME);
+    }
 
-  getCelebrateStep() {
-    return Math.floor(this.animationDuration / Constants.RHINO_CELEBRATE_TIME);
-  }
+    getCelebrateStep() {
+        return Math.floor(this.animationDuration / Constants.RHINO_CELEBRATE_TIME);
+    }
 
-  updateAsset() {
-    this.animationDuration++;
-    switch (this.mode) {
-      case Constants.RHINO_MODES.CHASE:
-        // Toggle chasing animation frames.
-        if (this.getChaseStep() >= Constants.RHINO_CHASE_ASSETS.length) {
-          this.animationDuration = 0;
+    updateAsset() {
+        this.animationDuration++;
+        switch (this.mode) {
+          case Constants.RHINO_MODES.CHASE:
+              // Toggle chasing animation frames.
+              if (this.getChaseStep() >= Constants.RHINO_CHASE_ASSETS.length) {
+                  this.animationDuration = 0;
+              }
+              this.assetName = Constants.RHINO_CHASE_ASSETS[this.getChaseStep()];
+              break;
+
+          case Constants.RHINO_MODES.EAT:
+              // Play through each of the eating animation frames.
+              if (this.getEatStep() >= Constants.RHINO_EAT_ASSETS.length) {
+                  // Celebrate after eating.
+                  this.setMode(Constants.RHINO_MODES.CELEBRATE);
+              }
+              this.assetName = Constants.RHINO_EAT_ASSETS[this.getEatStep()];
+              break;
+
+          case Constants.RHINO_MODES.CELEBRATE:
+              // Toggle celebrating animation frames.
+              if (this.getCelebrateStep() >= Constants.RHINO_CELEBRATE_ASSETS.length) {
+                 this.animationDuration = 0;
+              }
+              this.assetName = Constants.RHINO_CELEBRATE_ASSETS[this.getCelebrateStep()];
+              break;
         }
-        this.assetName = Constants.RHINO_CHASE_ASSETS[this.getChaseStep()];
-        break;
+    }
 
-      case Constants.RHINO_MODES.EAT:
-        // Play through each of the eating animation frames.
-        if (this.getEatStep() >= Constants.RHINO_EAT_ASSETS.length) {
-          // Celebrate after eating.
-          this.setMode(Constants.RHINO_MODES.CELEBRATE);
+    move(skierPosition) {
+        if (this.mode === Constants.RHINO_MODES.CHASE) {
+            this.speed = Math.min(this.speed * Constants.RHINO_SPEED_INCREASE_RATE, Constants.RHINO_MAX_SPEED);
+            this.moveTowardSkier(skierPosition);
         }
-        this.assetName = Constants.RHINO_EAT_ASSETS[this.getEatStep()];
-        break;
+        this.updateAsset();
+    }
 
-      case Constants.RHINO_MODES.CELEBRATE:
-        // Toggle celebrating animation frames.
-        if (this.getCelebrateStep() >= Constants.RHINO_CELEBRATE_ASSETS.length) {
-          this.animationDuration = 0;
+    moveTowardSkier(skierPosition) {
+        // Move toward the skier's position.
+        const { x: rhinoX, y: rhinoY } = this.getPosition();
+        const { x: skierX, y: skierY } = skierPosition;
+
+        const xDistance = skierX - rhinoX;
+        const yDistance = skierY - rhinoY;
+        const directDistance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
+
+        if (directDistance === 0) {
+           return
         }
-        this.assetName = Constants.RHINO_CELEBRATE_ASSETS[this.getCelebrateStep()];
-        break;
-    }
-  }
 
-  move(skierPosition) {
-    if (this.mode === Constants.RHINO_MODES.CHASE) {
-      this.speed = Math.min(this.speed * Constants.RHINO_SPEED_INCREASE_RATE, Constants.RHINO_MAX_SPEED);
-      this.moveTowardSkier(skierPosition);
-    }
-    this.updateAsset();
-  }
-
-  moveTowardSkier(skierPosition) {
-    // Move toward the skier's position.
-    const { x: rhinoX, y: rhinoY } = this.getPosition();
-    const { x: skierX, y: skierY } = skierPosition;
-
-    const xDistance = skierX - rhinoX;
-    const yDistance = skierY - rhinoY;
-    const directDistance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
-
-    if (directDistance === 0) {
-      return
+        this.y += Math.round(this.speed * yDistance / directDistance);
+        this.x += Math.round(this.speed * xDistance / directDistance);
     }
 
-    this.y += Math.round(this.speed * yDistance / directDistance);
-    this.x += Math.round(this.speed * xDistance / directDistance);
-  }
-
-  checkIfRhinoCaughtSkier(skier, assetManager) {
-    if (this.mode === Constants.RHINO_MODES.CHASE && intersectTwoRects(this.getBounds(assetManager), skier.getBounds(assetManager))) {
-      this.setMode(Constants.RHINO_MODES.EAT);
-      skier.hide();
-      skier.setMode(Constants.SKIER_MODES.EATEN);
+    checkIfRhinoCaughtSkier(skier, assetManager) {
+        if (this.mode === Constants.RHINO_MODES.CHASE && intersectTwoRects(this.getBounds(assetManager), skier.getBounds(assetManager))) {
+            this.setMode(Constants.RHINO_MODES.EAT);
+            skier.hide();
+            skier.setMode(Constants.SKIER_MODES.EATEN);
+        }
     }
-  }
 }
--- src/Entities/Skier.test.js
+++ src/Entities/Skier.test.js
@@ -4,86 +4,86 @@ import { Skier } from "./Skier"
 import { Rect } from "../Core/Utils";
 
 describe('setDirection', () => {
-  it('can not turn past left', () => {
-    const skier = new Skier(0,0);
-    const pastLeft = Constants.SKIER_DIRECTIONS.LEFT - 1;
-    skier.setDirection(pastLeft);
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
-  });
+    it('can not turn past left', () => {
+        const skier = new Skier(0,0);
+        const pastLeft = Constants.SKIER_DIRECTIONS.LEFT - 1;
+        skier.setDirection(pastLeft);
+        expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+    });
 
-  it('can not turn past right', () => {
-    const skier = new Skier(0,0);
-    const pastRight = Constants.SKIER_DIRECTIONS.RIGHT + 1;
-    skier.setDirection(pastRight);
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
-  });
+    it('can not turn past right', () => {
+        const skier = new Skier(0,0);
+        const pastRight = Constants.SKIER_DIRECTIONS.RIGHT + 1;
+        skier.setDirection(pastRight);
+        expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+    });
 });
 
 
 describe('checkIfSkierHitObstacle', () => {
-  let obstacleManager, assetManager;
-  beforeEach(() => {
-    obstacleManager = {
-      getObstacles: jest.fn().mockReturnValue([])
-    };
-    assetManager = {
-      getAsset: jest.fn().mockReturnValue({ width: 10, height: 10 })
-    };
-  });
+    let obstacleManager, assetManager;
+    beforeEach(() => {
+        obstacleManager = {
+           getObstacles: jest.fn().mockReturnValue([])
+        };
+        assetManager = {
+           getAsset: jest.fn().mockReturnValue({ width: 10, height: 10 })
+        };
+    });
 
-  function getCollidingObject(collisionObjectAssetName) {
-    return {
-      getPosition: jest.fn().mockReturnValue({ x: 0, y: 0 }),
-      width: 10,
-      height: 10,
-      getAssetName: jest.fn().mockReturnValue(collisionObjectAssetName),
-      getBounds: jest.fn().mockReturnValue(new Rect(-5, -5, 5, 5))
+    function getCollidingObject(collisionObjectAssetName) {
+        return {
+            getPosition: jest.fn().mockReturnValue({ x: 0, y: 0 }),
+            width: 10,
+            height: 10,
+            getAssetName: jest.fn().mockReturnValue(collisionObjectAssetName),
+            getBounds: jest.fn().mockReturnValue(new Rect(-5, -5, 5, 5))
+        }
     }
-  }
 
-  it('crashes on a collision with a tree', () => {
-    const skier = new Skier(0, 0);
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('tree') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
-  });
+    it('crashes on a collision with a tree', () => {
+        const skier = new Skier(0, 0);
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('tree') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
+    });
 
-  it('crashes on a collision with a tree while jumping', () => {
-    const skier = new Skier(0, 0);
-    skier.setMode(Constants.SKIER_MODES.JUMP);
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('tree') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
-  });
+    it('crashes on a collision with a tree while jumping', () => {
+        const skier = new Skier(0, 0);
+        skier.setMode(Constants.SKIER_MODES.JUMP);
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('tree') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
+    });
 
-  it('crashes on a collision with a rock', () => {
-    const skier = new Skier(0, 0);
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('rock1') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
-  });
+    it('crashes on a collision with a rock', () => {
+        const skier = new Skier(0, 0);
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('rock1') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.mode).toBe(Constants.SKIER_MODES.CRASH);
+    });
 
-  it('does not crash on a collision with a rock while jumping', () => {
-    const skier = new Skier(0, 0);
-    skier.setMode(Constants.SKIER_MODES.JUMP);
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('rock1') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.mode).not.toBe(Constants.SKIER_MODES.CRASH);
-  });
+    it('does not crash on a collision with a rock while jumping', () => {
+        const skier = new Skier(0, 0);
+        skier.setMode(Constants.SKIER_MODES.JUMP);
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('rock1') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.mode).not.toBe(Constants.SKIER_MODES.CRASH);
+    });
 
-  it('starts a jump on a collision with a jump ramp', () => {
-    const skier = new Skier(0, 0);
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('jumpRamp') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.mode).toBe(Constants.SKIER_MODES.JUMP);
-  });
+    it('starts a jump on a collision with a jump ramp', () => {
+        const skier = new Skier(0, 0);
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('jumpRamp') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.mode).toBe(Constants.SKIER_MODES.JUMP);
+    });
 
-  it('does not start a jump on a collision with a jump ramp while jumping', () => {
-    const skier = new Skier(0, 0);
-    skier.setMode(Constants.SKIER_MODES.JUMP);
-    jest.spyOn(skier, 'setMode');
-    obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('jumpRamp') ]);
-    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
-    expect(skier.setMode).not.toBeCalledWith(Constants.SKIER_MODES.JUMP);
-  });
+    it('does not start a jump on a collision with a jump ramp while jumping', () => {
+        const skier = new Skier(0, 0);
+        skier.setMode(Constants.SKIER_MODES.JUMP);
+        jest.spyOn(skier, 'setMode');
+        obstacleManager.getObstacles.mockReturnValue([ getCollidingObject('jumpRamp') ]);
+        skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+        expect(skier.setMode).not.toBeCalledWith(Constants.SKIER_MODES.JUMP);
+    });
 });
